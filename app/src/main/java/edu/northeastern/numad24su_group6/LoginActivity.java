3aa4c6ad1115997d2664d9d2cf53f3439f30d275
Annpq <princesummer98@gmail.com>
2024-07-20 18:58:39 +0800

    create new java file for LoginActivity

--- app/src/main/java/edu/northeastern/numad24su_group6/LoginActivity.java
+++ app/src/main/java/edu/northeastern/numad24su_group6/LoginActivity.java
@@ -48,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
         createUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
